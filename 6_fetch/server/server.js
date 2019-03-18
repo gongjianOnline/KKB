a85185b1981fdea1b74ad4192b6942b734a4b8fc
李承斌 <1307437149@qq.com>
2019-03-18 17:29:41 +0800

    2111

---
+++ 6_fetch/server/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const multiparty = require('multiparty')
+
+http.createServer((req,res)=>{
+	console.log("进入服务")
+	//注意：要提前定义好upload文件夹 否则会报错
+	let form = new multiparty.Form({uploadDir:'./upload/'})
+	form.parse( );
+	form.on("field",(name,value)=>{
+		console.log('field',name,value);
+	});
+	form.on("file",(name,file)=>{
+		console.log("file",name,file);
+	});
+	form.on('close',()=>{
+		console.log("结束");
+	});
+}).listen(8085)
