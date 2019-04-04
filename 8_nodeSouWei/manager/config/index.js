a1d8ef6b176a89a6190ec59607f467f0ada5c6ee
李承斌 <1307437149@qq.com>
2019-04-04 11:26:07 +0800

    测试sublime

--- 8_nodeSouWei/manager/config/index.js
+++ 8_nodeSouWei/manager/config/index.js
@@ -3,7 +3,7 @@ let mode = (process.env.OS == "Windows_NT" ?"dev":"prod");
 let dev = require("./config.dev");
 let prod = require("./config.prod");
 
-module.exports={
+module.exports = {
 	mode,
 	...(mode=="dev"?dev:prod)
 }
